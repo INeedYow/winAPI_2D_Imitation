08a973c0428b659f2bd649dfe1d53a53d90a9b8a
zmfhshtm258@naver.com <zmfhshtm258@naver.com>
2022-02-19 17:28:57 +0900

    새로 만듦

--- winAPI_2Dcopy/winAPI_2Dcopy.cpp
+++ winAPI_2Dcopy/winAPI_2Dcopy.cpp
@@ -7,9 +7,9 @@
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HINSTANCE   hInst;                                    // 현재 인스턴스입니다.
+WCHAR       szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR       szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
